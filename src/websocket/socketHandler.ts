57567dc5b4e59f6e9579d6ce4b378381b72437ff
SomnathKar000 <somnathkar201901@gmail.com>
2023-12-03 21:46:11 +0530

    new changes

--- src/websocket/socketHandler.ts
+++ src/websocket/socketHandler.ts
@@ -2,9 +2,15 @@ import * as WebSocket from "ws";
 
 function handleWebSocketConnection(socket: WebSocket) {
   console.log("client connected");
+
   socket.on("message", (data) => {
-    console.log("received: %s", data);
-    socket.send(`Hello, you sent -> ${data}`);
+    const message = JSON.parse(data.toString());
+    if (message.type === "join") {
+      socket.send(`Welcome ${message}`);
+    } else if (message.type === "message") {
+      console.log("received: %s", message);
+      socket.send(`Hello, you sent -> ${message.message}`);
+    }
   });
 
   socket.on("close", () => {
