fcf4cabda18273782fcd38933bf6265919aef4a5
SomnathKar000 <somnathkar201901@gmail.com>
2023-12-25 21:51:25 +0530

    added few code

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -1,13 +1,22 @@
 import { Response } from "express";
 import { AuthenticatedRequest } from "../services/UserService";
 import { getallMessages, newMessage } from "../services/messageService";
+import { CustomError } from "../middleware/errorHandling";
 
 const getAllMessages = (req: AuthenticatedRequest, res: Response) => {
   const { email } = req.body;
+  if (!email) {
+    throw new CustomError("Email is required", 400);
+  }
+  const messages = getallMessages(email);
 
   res
     .status(200)
-    .json({ success: true, message: "Successfully feched all messages" });
+    .json({
+      success: true,
+      message: "Successfully feched all messages",
+      messages,
+    });
 };
 
 const sendMessage = (req: AuthenticatedRequest, res: Response) => {
